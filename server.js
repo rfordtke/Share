4fb325bd67989995131107061c0247d1f1dd11f5
rfordtke <richard.ford@thyssenkrupp.com>
2015-06-16 11:35:02 -0400

    New test npm package

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require("http");
+var server = http.createServer(function(request, response) {
+response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title>Hello World Page</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("Hello World!");
+  response.write("</body>");
+  response.write("</html>");
+});
+ 
+server.listen(8009);
+console.log("Server is listening");
+
+
+
+exports.printMsg = function() {
+    console.log("This is a message from the demo package");
+}
