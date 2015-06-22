b59e6fdcf9af817452bfe5b832e8626d6b98d23d
rfordtke <richard.ford@thyssenkrupp.com>
2015-06-22 11:20:24 -0400

    Updates

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
-/*
-var http = require("http");
-var server = http.createServer(function(request, response) {
-response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Hello World Page</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("Hello World!");
-  response.write("</body>");
-  response.write("</html>");
-});
- 
-server.listen(8009);
-console.log("Server is listening");
 
+   'use_strict';
+    var requestify = require('requestify');
 
-*/
     exports.printMsg = function () {
         console.log("This is a message from the demo package");
     };
+
+    exports.getRequest = function(url,options){
+      if(url){
+          requestify.get(url,options).then(function(response){
+
+             console.log(response.getBody());
+          })
+      }
+   };
