f4f81f159352138a782de1bb4e1d91ea0573beaf
rfordtke <richard.ford@thyssenkrupp.com>
2015-06-22 14:44:57 -0400

    Updates

--- server.js
+++ server.js
@@ -6,10 +6,12 @@
         console.log("This is a message from the demo package");
     };
 
+
     exports.getRequest = function(url,options){
       if(url){
           requestify.get(url,options).then(function(response){
-             console.log(response.getBody());
+              console.log(response.getBody());
+              return response.getBody();
           })
       }
    };
