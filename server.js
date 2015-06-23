2a23a4b5766163c25d6d5fe1579b031ad56331bb
rfordtke <richard.ford@thyssenkrupp.com>
2015-06-23 14:47:01 -0400

    Updates

--- server.js
+++ server.js
@@ -2,11 +2,6 @@
    'use_strict';
     var requestify = require('requestify');
 
-    exports.printMsg = function () {
-        console.log("This is a message from the demo package");
-    };
-
-
     exports.getRequest = function(url,options){
       if(url){
           requestify.get(url,options).then(function(err,response){
