f56ad25b6814a1f139541fc2500e5be4bf3d0bd7
rfordtke <richard.ford@thyssenkrupp.com>
2015-06-25 09:51:16 -0400

    Updates

--- server.js
+++ server.js
@@ -2,15 +2,35 @@
    'use_strict';
     var requestify = require('requestify');
 
-    exports.getRequest = function(url,options){
+     function getRequest(url,options){
       if(url){
-          requestify.get(url,options).then(function(err,response){
+          requestify.get(url,options)
+              .then(function(err,response){
               if(err){
                   return err;
               }else {
-                  console.log(response.getBody());
+                  var obj = response.getBody();
+                  console.log(obj);
+                  console.log(obj.length)
+
                   return response.getBody();
               }
           })
       }
    };
+   function postData(url,dataObject){
+    if(url && dataObject){
+          requestify.post(url,dataObject)
+              .then(function(err,response){
+                  if(err){
+                      return err
+                  } else{
+                      return  response.getBody();
+                  }
+              })
+        }
+   }
+
+
+   module.exports = getRequest;
+   module.exports = postData;
