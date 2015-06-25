0fd7fbd6e03cc8d6a3f3b28b9a4752c1b64e66d6
rfordtke <richard.ford@thyssenkrupp.com>
2015-06-25 10:18:33 -0400

    Updates

--- server.js
+++ server.js
@@ -1,36 +1,33 @@
+'use_strict';
+var requestify = require('requestify');
 
-   'use_strict';
-    var requestify = require('requestify');
+exports.getRequest = function (url, options) {
+    if (url) {
+        requestify.get(url, options)
+            .then(function (err, response) {
+                if (err) {
+                    return err;
+                } else {
+                    var obj = response.getBody();
+                    console.log(obj);
+                    console.log(obj.length)
 
-     var getRequest = function (url,options){
-      if(url){
-          requestify.get(url,options)
-              .then(function(err,response){
-              if(err){
-                  return err;
-              }else {
-                  var obj = response.getBody();
-                  console.log(obj);
-                  console.log(obj.length)
-
-                  return response.getBody();
-              }
-          })
-      }
-   };
-  var postData = function(url,dataObject){
-    if(url && dataObject){
-          requestify.post(url,dataObject)
-              .then(function(err,response){
-                  if(err){
-                      return err
-                  } else{
-                      return  response.getBody();
-                  }
-              })
-        }
-   }
+                    return response.getBody();
+                }
+            })
+    }
+};
+exports.postData = function (url, dataObject) {
+    if (url && dataObject) {
+        requestify.post(url, dataObject)
+            .then(function (err, response) {
+                if (err) {
+                    return err
+                } else {
+                    return response.getBody();
+                }
+            })
+    }
+}
 
 
-   module.exports = getRequest;
-   module.exports = postData;
